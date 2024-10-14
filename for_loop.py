9076b0e3ec89cb18ce8501f73dfa586b965a617b
jeshika311 <jeshikatha@gmail.com>
2024-10-14 14:27:25 +0530

    apply different operations in for loop

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,11 @@
 fruits = ["apple", "banana", "orange"]
 color = ["red" , "yellow" , "orange"]
+
+#print all elements 
+for x in fruits:
+   print(x)
+
+#nested loop
 for x in fruits:
   for y in color:
      print(x,y)
