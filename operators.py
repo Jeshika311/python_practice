dac5be96006b180cf7dda8cf2d2a8bee4db89c81
jeshika311 <jeshikatha@gmail.com>
2024-10-18 19:39:49 +0530

    add new file

---
+++ operators.py
@@ -0,0 +1,20 @@
+#Arithmetic operator
+x=10
+y=20
+z=30
+w=25
+
+sum = x+y
+subtract = y-x
+divide = y/x
+multiply = x*y
+modulas = x%y
+floor = y//x
+exponential = x**y
+print(sum)
+print(subtract)
+print(divide)
+print(multiply)
+print(modulas)
+print(floor)
+print(exponential)
