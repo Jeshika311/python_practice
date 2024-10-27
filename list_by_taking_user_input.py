8eb8062f5f6d9bd67799d1c68218032a58149a48
jeshika311 <jeshikatha@gmail.com>
2024-10-27 15:41:07 +0530

    add new file

---
+++ list_by_taking_user_input.py
@@ -0,0 +1,8 @@
+list = []
+
+list.append(input("Enter value1 :- "))
+list.append(input("Enter value2 :- "))
+list.append(input("Enter value3 :- "))
+list.append(input("Enter value4 :- "))
+
+print(list)
