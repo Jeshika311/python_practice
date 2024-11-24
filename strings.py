4d17bfa544f2daa31795f83ec2f15a75fe8e30e0
jeshika311 <jeshikatha@gmail.com>
2024-11-24 09:22:11 +0530

    Apply different operations on string

--- strings.py
+++ strings.py
@@ -1,4 +1,11 @@
-#Find length of the string
+#we can write strings in single , double or triple quotes
 
-a = " Hello, World! "
-print(len(a))
+a = "It's alright"  #Assigning string to a Variable
+print(a)
+
+print("He is called 'Juhi'") #Display string literal with the print() function
+
+#slicing strings
+
+b = "Hello, World!"  #slicing by positive indexing
+print(b[2:5])
