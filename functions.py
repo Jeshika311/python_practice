b9e5455caafe17f4afff25387425cdd0e962f68e
jeshika311 <jeshikatha@gmail.com>
2024-11-27 00:24:00 +0530

    Apply different operations on function

--- functions.py
+++ functions.py
@@ -4,3 +4,12 @@ def my_function():
   print("Hello from a function")
 
 my_function()
+
+#Pass Arguments from function
+
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
