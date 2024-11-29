fce67dd1f5372bf64278ba26f6233978d5b4965c
jeshika311 <jeshikatha@gmail.com>
2024-11-29 18:47:50 +0530

    Apply different operations on a string

--- functions.py
+++ functions.py
@@ -36,3 +36,16 @@ my_function("Brazil")
 def my_function(food):
   for x in food:
     print(x)
+
+fruits = ["apple", "banana", "cherry"]
+
+my_function(fruits)
+
+#Return Values
+
+def my_function(x):
+  return 5 * x
+
+print(my_function(3))
+print(my_function(5))
+print(my_function(9))
