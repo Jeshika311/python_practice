7eb14ae3e774862b1e2c5b4a7cf586624aa09974
jeshika311 <jeshikatha@gmail.com>
2024-12-04 10:40:11 +0530

    Different operations on set

--- sets.py
+++ sets.py
@@ -2,3 +2,9 @@
 
 set = {"apple" , "banana" , "cheery"}
 print(set)
+
+#True and 1 are considered the same value in sets
+#False and 2 are considered the same value in sets
+
+set = {"apple" , "banana" , "cheery" , True , False , 1 , 0 ,2}
+print(set)
