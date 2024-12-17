831f879f0c47146a146655d99e693aa47631c765
jeshika311 <jeshikatha@gmail.com>
2024-12-17 08:14:29 +0530

    Apply different operations on a set

--- sets.py
+++ sets.py
@@ -80,3 +80,11 @@ set1 = {"a" ,"b" , "c"}
 set2 ={1, 2, 3}
 set3 = set1 | set2
 print(set3)
+
+#join multiple sets 
+
+set1 = {"a" ,"b" , "c"}
+set2 = {1, 2, 3}
+set3 = {"apple" , "banana" , "cheery"}
+set4 = set1.union(set2,set3)
+print(set4)
