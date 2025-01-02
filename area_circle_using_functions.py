4a043178d96440f681468703fe1c86c5db22a32e
jeshika311 <jeshikatha@gmail.com>
2025-01-02 15:53:38 +0530

    add nre file

---
+++ area_circle_using_functions.py
@@ -0,0 +1,9 @@
+import math
+
+def area_of_circle(radius):
+    area = math.pi * radius ** 2  # Formula for area of a circle: A = π * r^2
+    return area
+
+radius = float(input("Enter the radius of the circle: "))
+area = area_of_circle(radius)
+print(f"The area of the circle with radius {radius} is: {area}")
